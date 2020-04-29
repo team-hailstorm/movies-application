24d28a2028bb9d47ee1462bf1be1377671c90f9c
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-04-29 12:15:54 -0500

    Feat: created buttons for edit, still in process of creating a function to edit movie post.

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ const {getMovies, createMovie, testMovie} = require('./api.js');
 
 $(document).ready( () => {
 
-  $('#submit-button').on('click', function(e) {
-    e.preventDefault();
+    //Add Movie
+  $('#submit-button').on('click', function() {
     let titleValue = $('#movie-title').val();
     let ratingValue = $('#movie-rating').val();
 
@@ -25,32 +25,36 @@ $(document).ready( () => {
    getMovies();
   });
 
+  // Edit Movie
+    $('#edit-button').on('click', function (e) {
+        e.preventDefault();
+        console.log('hello');
+    });
+
+
   getMovies().then((movies) => {
     console.log('Here are all the movies:');
     let movie = ``;
     movies.forEach(({title, rating, id}) => {
       console.log(`id#${id} - ${title} - rating: ${rating}`);
       movie += `
-          <ul>
+          <ul >
             <li>ID: ${id}</li>
             <li>Title: ${title}</li>
             <li>Rating: ${rating}</li>
+            <form>
+                <button id="edit-button">Edit</button>
+                <button>Delete</button>
+            </form>
           </ul>
       
       `;
-
-
     });
     $('#movie-display').html(movie);
     $('#loading').css('display', 'none');
-
-
-
-
   }).catch((error) => {
-    // alert('Oh no! Something went wrong.\nCheck the console for details.')
     console.log(error);
-  })
+  });
 
 
 });
