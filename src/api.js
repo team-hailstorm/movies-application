2a249146e50f8ad45e85b4529be33fa45edb9fb5
Moriah Humphries <moriah.humphries@gmail.com>
2020-04-29 10:17:34 -0500

    added post request to add user submitted movies

--- src/api.js
+++ src/api.js
@@ -1,9 +1,29 @@
+const testMovie = {
+  "title": "The Other Test Movie",
+      "rating": "3",
+
+};
+
 module.exports = {
   getMovies: () => {
     return fetch('/api/movies')
       .then(response => response.json());
   },
 
+
+
+    createMovie: () => {
+      fetch("/api/movies", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(testMovie),
+      })
+          .then( response => response.json() )
+          .then( data => console.log(data) )
+          .catch( error => console.error(error));
+    }
 
 
 };
